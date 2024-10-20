e6c67147bb1929771dd9235a28cf4bb3bc1bf074
Marco Carvalho <maacnet@hotmail.com>
2024-10-20 18:38:50 -0300

    add

---
+++ teste.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+    return a + b
+
+# Para executar a função e obter o resultado, siga os passos abaixo:
+
+# 1. Salve este arquivo como `teste.py`.
+# 2. Abra um terminal ou prompt de comando.
+# 3. Navegue até o diretório onde o arquivo `teste.py` está salvo.
+#    Por exemplo, se estiver no Windows, você pode usar o comando:
+#    cd /d:/Projetos de Estudos/pythonfundamentos/
+# 4. Execute o script usando o Python:
+#    python teste.py
+# 5. No script, você pode adicionar uma chamada à função `soma` e imprimir o resultado:
+if __name__ == "__main__":
+    resultado = soma(3, 5)
+    print(f"O resultado da soma é: {resultado}")
